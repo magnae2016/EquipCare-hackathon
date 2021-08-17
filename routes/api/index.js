4f833d96f0da2727c951c64e8db892f2e6df6ff4
magnae2016 <magnae2016@gmail.com>
2021-08-17 21:58:02 +0900

    Add /api/alarms

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 var express = require("express");
 var router = express.Router();
 var equipmentRouter = require("./equipment");
+var alarmsRouter = require("./alarms");
 
 router.use("/equipment", equipmentRouter);
+router.use("/alarms", alarmsRouter);
 
 module.exports = router;
