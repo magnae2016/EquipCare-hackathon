61ef3dc67412dcd7a2875f73748981bbe259e1d6
magnae2016 <magnae2016@gmail.com>
2021-08-18 02:05:38 +0900

    Add /api/metadata/alarms

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,10 @@ var express = require("express");
 var router = express.Router();
 var equipmentRouter = require("./equipment");
 var alarmsRouter = require("./alarms");
+var metadataRouter = require("./metadata");
 
 router.use("/equipment", equipmentRouter);
 router.use("/alarms", alarmsRouter);
+router.use("/metadata", metadataRouter);
 
 module.exports = router;
---
+++ routes/api/metadata.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var router = express.Router();
+var pool = require("../../module/db");
+
+/* GET /api/metadata/alarms */
+router.get("/alarms", async (req, res, next) => {
+    const SQL = `
+        SELECT EVENT_NAME, count(*) as COUNT
+        FROM
+            events
+        WHERE
+            EVENT_TYPE = 'Alarm'
+                AND RESERVED_1 IN (2 , 3)
+        GROUP BY EVENT_NAME;
+    `;
+    const [rows, fields] = await pool.query(SQL);
+    res.json(rows);
+});
+
+module.exports = router;
